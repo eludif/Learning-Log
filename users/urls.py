864432a29ae762eddf7373282b1f0f980632af30
Fiyinfolu <faeludire@gmail.com>
2017-12-17 07:28:38 +0100

    Add Registration page

--- users/urls.py
+++ users/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
 
     # Logout page
     url(r'^logout/$', views.logout_view, name='logout'),
+
+    # Registration page
+    url(r'register/$', views.register, name='register'),
+
 ]
