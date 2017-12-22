7302ea733788c44c5322df4456f82a2c5fd65f3f
faeludire <faeludire@gmail.com>
2017-12-22 11:03:11 +0100

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -149,7 +149,7 @@ if os.getcwd() == '/app':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = '/static/'
+    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
